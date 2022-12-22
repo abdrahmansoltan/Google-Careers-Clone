b15e6131a9451cd0775ad15a0e3b0ec204984e94
Abdelrahman Soltan <asoltan@instabug.com>
2022-12-22 09:29:42 +0200

    feat(App Configuration): :wrench: configure formatting, linting and jest (testing) settings

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"], // file extensions to look for when testing are ".spec.js or .test.js"
 };
