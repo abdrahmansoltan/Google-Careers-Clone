b0887bfd48ffdde955ea42b7d704e16b7d876c2a
Abdelrahman Soltan <asoltan@instabug.com>
2022-12-22 19:36:04 +0200

    style(Tailwind): :lipstick: add open-sans fonts and extend tailwind font & color Theme properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme"); // import the js-theme-object
+
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // extend some tailwind properties
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans], // add the open-sans font from https://fonts.google.com/
+      },
+      colors: {
+        // create custom color classes
+        "brand-gray-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+      },
+    },
   },
   plugins: [],
 };
