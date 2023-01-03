4d79fb81863dd72741ff4cce5411d39784a03209
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-03 10:56:37 +0200

    feat: :sparkles: create action-button and profile-image components

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    :src="imageLink"
+    alt="Profile Image"
+    class="w-8 h-8 object-contain rounded-3xl"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://images.generated.photos/EDENxeInP9XQAFYpKVq-tD8fOqdzawrOW7Xmghb-2ic/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzcwMjA2LmpwZw.jpg",
+    };
+  },
+};
+</script>
