5ec85639991288bb3185062c763d6ea149849214
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-04 08:57:40 +0200

    feat: :heavy_plus_sign: add fontawesome dependencies and register its component globally

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from "vue";
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { createApp } from "vue";
 
-createApp(App).mount("#app");
+// registering search-icon with the library
+library.add(faSearch); // register it to be able to use it as :icon="search"
+
+// Registering Global Components
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
