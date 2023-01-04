ccf90c43a08ac6080f7e2e210425bbad75e6b740
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-04 08:34:53 +0200

    test: :test_tube: assert on rendering ActionButton data correctly

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,25 @@
+import ActionButton from "@/components/ActionButton.vue";
+import { mount } from "@vue/test-utils";
+
+describe("ActionButton", () => {
+  it("renders text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "I'm clickable",
+        type: "primary",
+      },
+    });
+    expect(wrapper.text()).toMatch("I'm clickable");
+  });
+
+  it("applyies one of several styles to button", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "I'm clickable",
+        type: "primary",
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.classes("primary")).toBe(true);
+  });
+});
