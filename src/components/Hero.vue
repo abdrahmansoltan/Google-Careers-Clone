a9d8bfc81999a7705c5a5217a37aa99cc844f882
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-07 10:34:39 +0200

    feat: add hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-white">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1"></div>
+        <div class="col-start-2 col-span-5">
+          <head-line />
+        </div>
+        <div class="col-start-7 col-span-5">5 colum</div>
+        <div class="col-start-12 col-span-1"></div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import HeadLine from "@/components/HeadLine.vue";
+export default {
+  name: "Hero",
+  components: { HeadLine },
+  data() {
+    return {
+      onJobResultsPage: true,
+    };
+  },
+};
+</script>
