e4c3609e16ab3affeb5f9d207dd10060a0659b0d
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-08 10:02:55 +0200

    test: :test_tube: use RouterLinkStub to stub "router-link" + create mounting factory function with $route mocking

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -1,14 +1,22 @@
 import MainNav from "@/components/Navigation/MainNav.vue";
-import { shallowMount } from "@vue/test-utils";
+import { RouterLinkStub, shallowMount } from "@vue/test-utils";
 
 describe("MainNav", () => {
+  const createConfig = {
+    global: {
+      stubs: {
+        "router-link": RouterLinkStub,
+      },
+    },
+  };
+
   it("displays company name", () => {
-    const wrapper = shallowMount(MainNav);
+    const wrapper = shallowMount(MainNav, createConfig);
     expect(wrapper.text()).toMatch("Google Careers"); // check if this text is anywhere in the wrapper
   });
 
   it("displays menu items for navigation", () => {
-    const wrapper = shallowMount(MainNav);
+    const wrapper = shallowMount(MainNav, createConfig);
     const navigationMenuItem = wrapper.findAll(
       "[data-test='main-nav-list-item']"
     );
@@ -25,7 +33,7 @@ describe("MainNav", () => {
 
   describe("when user is logged out", () => {
     it("prompts user to sign in", () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig);
       const loginButton = wrapper.find("[data-test='login-button']");
       expect(loginButton.exists()).toBe(true);
     });
@@ -33,7 +41,7 @@ describe("MainNav", () => {
 
   describe("when user logs in", () => {
     it("displays user profile picture", async () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig);
       let profileImage = wrapper.find("[data-test='profile-image']");
       expect(profileImage.exists()).toBe(false);
 
@@ -47,7 +55,7 @@ describe("MainNav", () => {
     });
 
     it("displays sub navigation menu with additional information", async () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig);
       let subnav = wrapper.find("[data-test='subnav']");
       expect(subnav.exists()).toBe(false);
 
--- tests/unit/components/Navigation/SubNav.test.js
+++ tests/unit/components/Navigation/SubNav.test.js
@@ -2,22 +2,25 @@ import SubNav from "@/components/Navigation/SubNav.vue";
 import { mount } from "@vue/test-utils";
 
 describe("SubNav", () => {
+  const createConfig = (routeName) => ({
+    // stub for global components
+    global: {
+      stubs: {
+        FontAwesomeIcon: true,
+      },
+      mocks: {
+        // mocks "this.$route object"
+        $route: {
+          name: routeName,
+        },
+      },
+    },
+  });
+
   describe("when user is on job page", () => {
     it("displays job count", () => {
-      const wrapper = mount(SubNav, {
-        // stub for global components
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            // Temporary for TDD
-            onJobResultsPage: true,
-          };
-        },
-      });
+      const routeName = "JobResults";
+      const wrapper = mount(SubNav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(true);
     });
@@ -25,19 +28,8 @@ describe("SubNav", () => {
 
   describe("when user is not on job page", () => {
     it("doesn't display job count", () => {
-      const wrapper = mount(SubNav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            // Temporary for TDD
-            onJobResultsPage: false,
-          };
-        },
-      });
+      const routeName = "Home";
+      const wrapper = mount(SubNav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(false);
     });
