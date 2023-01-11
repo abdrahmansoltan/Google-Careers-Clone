f026735e005a06b8fa6393db14d7d2929f1bdb31
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-11 07:28:54 +0200

    config: 🔧 add "axios" to the  jest  Ignored Patterns

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"], // file extensions to look for when testing are ".spec.js or .test.js"
+  transformIgnorePatterns: ["node_modules/(?!axios)"],
 };
