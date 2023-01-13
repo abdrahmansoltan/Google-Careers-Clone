b5a2f48426a4367441c38947236ea7e334b5680a
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-13 10:10:17 +0200

    feat: :lipstick: add fontawesome icons & register them to global component

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { createApp } from "vue";
 import router from "@/router";
 import store from "@/store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faAngleDown,
+  faAngleUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { createApp } from "vue";
 
-// registering search-icon with the library
-library.add(faSearch); // register it to be able to use it as :icon="search"
+// registering icons with the library
+library.add(faSearch, faAngleUp, faAngleDown); // register it to be able to use it as :icon="search"
 
 // Registering Global Components
 createApp(App)
