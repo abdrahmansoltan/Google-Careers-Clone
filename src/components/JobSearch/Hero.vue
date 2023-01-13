15d5e00d11ec95bd75191c12163ed382df7db5b6
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-13 14:29:02 +0200

    feat: :sparkles: create Spotlight dynamic component with scoped slots + updated "db.json"

--- src/components/JobSearch/Hero.vue
+++ src/components/JobSearch/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-white">
+  <main class="flex flex-col h-screen">
+    <section class="flex flex-col pt-10 pb-20 bg-white">
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1"></div>
         <div class="col-start-2 col-span-5">
@@ -16,15 +16,42 @@
         <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
+
+    <Spotlight class="flex flex-row justify-center pb-16">
+      <template #default="{ img, title, description }">
+        <router-link
+          to="/jobs/results"
+          class="flex flex-col mx-5 border rounded-lg w-72 bg-brand-gray-2"
+        >
+          <img :src="img" class="object-contain" />
+          <div class="h-48 px-6 py-4 mt-3">
+            <h3 class="text-lg font-medium">
+              {{ title }}
+            </h3>
+            <p class="mt-3 text-sm">
+              {{ description }}
+            </p>
+          </div>
+
+          <router-link
+            to="/jobs/results"
+            class="px-6 pb4 text-sm text-brand-blue-1"
+          >
+            See jobs
+          </router-link>
+        </router-link>
+      </template>
+    </Spotlight>
   </main>
 </template>
 
 <script>
 import HeadLine from "@/components/JobSearch/HeadLine.vue";
+import Spotlight from "@/components/JobSearch/Spotlight.vue";
 import JobSearchForm from "./JobSearchForm.vue";
 export default {
   name: "Hero",
-  components: { HeadLine, JobSearchForm },
+  components: { HeadLine, JobSearchForm, Spotlight },
   data() {
     return {
       onJobResultsPage: true,
