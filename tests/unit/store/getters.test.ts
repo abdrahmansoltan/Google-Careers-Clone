e38d9de68291586cdc1258e647872267396dd009
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-17 17:46:27 +0200

    refactor(Typescript): :test_tube: convert getters and mutations test files to typescript with factory functions

--- tests/unit/store/getters.test.ts
+++ tests/unit/store/getters.test.ts
@@ -1,15 +1,15 @@
 import getters from "@/store/getters";
+import { createJob, createState } from "./utils";
 
 describe("getters", () => {
   describe("UNIQUE_ORGANIZATIONS", () => {
     it("finds unique organizations from list of jobs", () => {
-      const state = {
-        jobs: [
-          { organization: "Google" },
-          { organization: "Amazon" },
-          { organization: "Google" },
-        ],
-      };
+      const jobs = [
+        createJob({ organization: "Google" }),
+        createJob({ organization: "Amazon" }),
+        createJob({ organization: "Google" }),
+      ];
+      const state = createState({ jobs });
       const result = getters.UNIQUE_ORGANIZATIONS(state);
       expect(result).toEqual(new Set(["Google", "Amazon"]));
     });
@@ -17,13 +17,12 @@ describe("getters", () => {
 
   describe("UNIQUE_JOB_TYPES", () => {
     it("finds unique job types from list of jobs", () => {
-      const state = {
-        jobs: [
-          { jobType: "Full-time" },
-          { jobType: "Temporary" },
-          { jobType: "Full-time" },
-        ],
-      };
+      const jobs = [
+        createJob({ jobType: "Full-time" }),
+        createJob({ jobType: "Temporary" }),
+        createJob({ jobType: "Full-time" }),
+      ];
+      const state = createState({ jobs });
       const result = getters.UNIQUE_JOB_TYPES(state);
       expect(result).toEqual(new Set(["Full-time", "Temporary"]));
     });
@@ -33,20 +32,18 @@ describe("getters", () => {
   describe("INCLUDE_JOB_BY_ORGANIZATION", () => {
     describe("when the user hasn't selected any organizations", () => {
       it("includes job", () => {
-        const state = {
-          selectedOrganizations: [],
-        };
-        const job = { organization: "Google" };
+        const state = createState({ selectedOrganizations: [] });
+        const job = createJob({ organization: "Google" });
         const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
         expect(includeJob).toBe(true);
       });
     });
 
     it("identifies if job is associated with given organizations", () => {
-      const state = {
+      const state = createState({
         selectedOrganizations: ["Google", "Amazon"],
-      };
-      const job = { organization: "Google" };
+      });
+      const job = createJob({ organization: "Google" });
       const includeJob = getters.INCLUDE_JOB_BY_ORGANIZATION(state)(job);
       expect(includeJob).toBe(true);
     });
@@ -55,20 +52,20 @@ describe("getters", () => {
   describe("INCLUDE_JOB_BY_JOB_TYPE", () => {
     describe("when the user hasn't selected any job type", () => {
       it("includes job", () => {
-        const state = {
+        const state = createState({
           selectedJobTypes: [],
-        };
-        const job = { jobType: "Full-time" };
+        });
+        const job = createJob({ jobType: "Full-time" });
         const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
         expect(includeJob).toBe(true);
       });
     });
 
     it("identifies if job is associated with given job types", () => {
-      const state = {
+      const state = createState({
         selectedJobTypes: ["Full-time", "Part-time"],
-      };
-      const job = { jobType: "Part-time" };
+      });
+      const job = createJob({ jobType: "Part-time" });
       const includeJob = getters.INCLUDE_JOB_BY_JOB_TYPE(state)(job);
       expect(includeJob).toBe(true);
     });
@@ -83,10 +80,10 @@ describe("getters", () => {
         INCLUDE_JOB_BY_ORGANIZATION,
         INCLUDE_JOB_BY_JOB_TYPE,
       };
-      const job = { organization: "Google", jobType: "Full-time" };
-      const state = {
+      const job = createJob({ organization: "Google", jobType: "Full-time" });
+      const state = createState({
         jobs: [job],
-      };
+      });
 
       const result = getters.FILTERED_JOBS(state, mockGetters);
       expect(INCLUDE_JOB_BY_ORGANIZATION).toHaveBeenCalledWith(job);
---
+++ tests/unit/store/mutations.test.ts
@@ -0,0 +1,43 @@
+import mutations from "@/store/mutations";
+import { createJob, createState } from "./utils";
+
+describe("mutations", () => {
+  describe("LOGIN_USER", () => {
+    it("logs the user in", () => {
+      const startingState = createState({ isLoggedIn: false });
+      mutations.LOGIN_USER(startingState);
+      expect(startingState.isLoggedIn).toBe(true);
+    });
+  });
+
+  describe("RECEIVE_JOBS", () => {
+    it("receives jobs from API response", () => {
+      const startingState = createState({ jobs: [] });
+      const jobOne = createJob();
+      const jobTwo = createJob();
+      mutations.RECEIVE_JOBS(startingState, [jobOne, jobTwo]);
+      expect(startingState.jobs).toEqual([jobOne, jobTwo]);
+    });
+  });
+
+  describe("ADD_SELECTED_ORGANIZATIONS", () => {
+    it("updates organizations that the user has chosen to filter jobs by", () => {
+      const startingState = createState({ selectedOrganizations: [] });
+      const data = ["Org1", "Org2"];
+      mutations.ADD_SELECTED_ORGANIZATIONS(startingState, data);
+      expect(startingState.selectedOrganizations).toEqual(["Org1", "Org2"]);
+    });
+  });
+
+  describe("ADD_SELECTED_JOB_TYPES", () => {
+    it("updates job types that the user has chosen to filter jobs by", () => {
+      const startingState = createState({ selectedJobTypes: [] });
+      const data = ["Full-time", "Part-time"];
+      mutations.ADD_SELECTED_JOB_TYPES(startingState, data);
+      expect(startingState.selectedJobTypes).toEqual([
+        "Full-time",
+        "Part-time",
+      ]);
+    });
+  });
+});
