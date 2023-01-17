efc580799e667d8264687825d164226c301861e0
Abdelrahman Soltan <asoltan@instabug.com>
2023-01-17 17:44:16 +0200

    refactor(Typescript): :art: convert store getters & mutations to typescript

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,3 +1,4 @@
+import { Job } from "@/api/types";
 import {
   FILTERED_JOBS,
   INCLUDE_JOB_BY_JOB_TYPE,
@@ -5,19 +6,26 @@ import {
   UNIQUE_JOB_TYPES,
   UNIQUE_ORGANIZATIONS,
 } from "@/store/constants";
+import GlobalState from "@/store/types";
+
+// for the getters parameter in the helper-getter-function
+interface IncludeJobGetters {
+  INCLUDE_JOB_BY_ORGANIZATION: (job: Job) => boolean;
+  INCLUDE_JOB_BY_JOB_TYPE: (job: Job) => boolean;
+}
 
 const getters = {
-  [UNIQUE_ORGANIZATIONS](state) {
-    const uniqueOrganizations = new Set();
+  [UNIQUE_ORGANIZATIONS](state: GlobalState) {
+    const uniqueOrganizations = new Set<string>();
     state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
     return uniqueOrganizations;
   },
-  [UNIQUE_JOB_TYPES](state) {
-    const uniqueJobTypes = new Set();
+  [UNIQUE_JOB_TYPES](state: GlobalState) {
+    const uniqueJobTypes = new Set<string>();
     state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
     return uniqueJobTypes;
   },
-  [FILTERED_JOBS](state, getters) {
+  [FILTERED_JOBS](state: GlobalState, getters: IncludeJobGetters) {
     // using helper-getters functions
     return state.jobs
       .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
@@ -26,11 +34,11 @@ const getters = {
 
   // --------- HELPER GETTERS ------------ //
   // a getter that is a function that return value, so that we can use it in other getters
-  [INCLUDE_JOB_BY_ORGANIZATION]: (state) => (job) => {
+  [INCLUDE_JOB_BY_ORGANIZATION]: (state: GlobalState) => (job: Job) => {
     if (state.selectedOrganizations.length === 0) return true;
     return state.selectedOrganizations.includes(job.organization);
   },
-  [INCLUDE_JOB_BY_JOB_TYPE]: (state) => (job) => {
+  [INCLUDE_JOB_BY_JOB_TYPE]: (state: GlobalState) => (job: Job) => {
     if (state.selectedJobTypes.length === 0) return true;
     return state.selectedJobTypes.includes(job.jobType);
   },
